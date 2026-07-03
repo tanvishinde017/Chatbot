8392a943ea853029708ec532f57a0e4fc52c0a41
tanvi shinde <tanvishinde1560@gmail.com>
2026-07-03 12:27:34 +0530

    Update app.run to specify host and port

--- app.py
+++ app.py
@@ -27,4 +27,8 @@ def chat():
     return jsonify({"reply": bot_reply})
 
 if __name__ == "__main__":
-    app.run(debug=Config.DEBUG)
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        debug=True
+    )
